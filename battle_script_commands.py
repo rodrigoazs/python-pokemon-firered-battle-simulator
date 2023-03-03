3a3e09cde7d55f765b9aaf2fbd1b095bf645e7b1
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2023-03-03 05:29:38 -0300

    add random to damage

---
+++ battle_script_commands.py
@@ -0,0 +1,10 @@
+import random
+
+
+# Multiplies the damage by a random factor between 85% to 100% inclusive
+# TODO: int can be applied only in the end of calculation
+def apply_random_dmg_multiplier(damage):
+    if not damage:
+        return damage
+    damage = damage * random.randint(0, 15) / 100
+    return max([1, damage])
